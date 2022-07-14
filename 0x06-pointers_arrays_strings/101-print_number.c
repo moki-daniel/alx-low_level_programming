b7440491f012fda9baea5108d5b3d9bba68b4b80
moki-daniel <mokidnl@gmail.com>
2022-07-14 13:35:42 -0700

    number 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,16 @@
  *Return: nothing 
  */ 
 
-void print_number(int n) 
-{ 
-        unsigned int x = n; 
- 
-        if (n < 0) 
-        { 
-                _putchar('-'); 
-                x = -x; 
-        } 
-        if ((x / 10) > 0) 
-                print_number(x / 10); 
-        _putchar((x % 10) + '0'); 
+void print_number(int n)
+{
+	unsigned int x = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
 
