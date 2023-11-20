5608715a081a27542272a981b852729466ca0b15
root <user.name>
2023-11-20 09:07:10 -0800

    Write a function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,23 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - Prints all elements of a list
- * @h: Pointer to a list
+ * print_listint - prints all the elements of a listint_t list.
  *
- * Return: Number of nodes
+ * @h: head of linklist node
+ *
+ * Return: the number of nodes
+ *
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		++i;
+		count++;
 	}
-	return (i);
+	return (count);
 }
